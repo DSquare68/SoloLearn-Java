a0f53d6249620af05d590d253ba98be3aa72ebac
DanielDob <35843305+DanielDob@users.noreply.github.com>
2018-01-28 20:49:47 +0100

    adding bat file

---
+++ matrix.bat
@@ -0,0 +1,4 @@
+@chcp 1250
+javac -d bin -cp bin src/Main/Matrix.java
+java -cp bin Main.Matrix   
+set /p key=Enter key:
---
+++ sinus.bat
@@ -0,0 +1,4 @@
+﻿@chcp 1250
+javac -d bin -cp bin src/Main/Sinus.java
+java -cp bin Main.Sinus   
+set /p key=Enter key:
